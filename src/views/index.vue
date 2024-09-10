26392b325d0b3f48a52344293a5e6c202e51203f
Xinxuan Zhuo <1575165952@qq.com>
2024-09-10 15:48:39 +0800

    add:初始化云聚Club管理后台，基础功能开发完毕

---
+++ src/views/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>This is Index</div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped lang="scss"></style>
